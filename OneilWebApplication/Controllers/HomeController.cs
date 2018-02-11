4ec1239b238860d5f095f970a4ca173a421be878
PatONeil27 <36377792+PatONeil27@users.noreply.github.com>
2018-02-11 12:53:05 -0500

    Add project files.

---
+++ OneilWebApplication/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OneilWebApplication.Classes;
+using OneilWebApplication.Entities;
+
+namespace OneilWebApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            User _user = new User
+            {
+                FirstName = "Bob",
+                LastName = "Smith",
+                Id = 2,
+                LoginName = "bsmith"
+            };
+            using(EntitiesOneilWebInterfaces _ef = new EntitiesOneilWebInterfaces())
+            {
+                _ef.Users.Add(_user);
+                _ef.SaveChanges();
+            }
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ OneilWebApplication/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="OneilWebApplication.MvcApplication" Language="C#" %>
